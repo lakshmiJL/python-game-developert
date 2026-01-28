6f827100c41e1b345e6d8ced6ee5dbe6dd6fa743
Lakshmi Priya T <141428624+lakshmiJL@users.noreply.github.com>
2026-01-28 21:26:24 +0530

    Create Homework.py

---
+++ Homework.py
@@ -0,0 +1,3 @@
+#Create one actor that moves to where you click. Each time you click, the actor changes to a different image 
+#use vector.image = "imagename"
+#Students practice mouse events and changing actor attributes
